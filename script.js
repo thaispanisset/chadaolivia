300f6c2b8b4476204a1f12cd0f2201dbe239150b
Thais Panisset <panisset.thais@gmail.com>
2022-07-22 15:37:04 -0300

    Projeto finalizado do sorteador

---
+++ script.js
@@ -0,0 +1,615 @@
+let data = {
+    "participantes" : [
+        {
+            "numero": "1",
+            "nome": "Luciane"
+        },
+        {
+            "numero": "2",
+            "nome": "Tiago Frossard"
+        },
+        {
+            "numero": "3",
+            "nome": "Eldio - @eldius"
+        },
+        {
+            "numero": "4",
+            "nome": "Elizangela - Mendes"
+        },
+        {
+            "numero": "5",
+            "nome": "Cacilda"
+        },
+        {
+            "numero": "6",
+            "nome": "Michelli Trindade"
+        },
+        {
+            "numero": "7",
+            "nome": "Samia - @snfwr_"
+        },
+        {
+            "numero": "8",
+            "nome": "Michelli Trindade"
+        },
+        {
+            "numero": "9",
+            "nome": "Izabelle"
+        },
+        {
+            "numero": "10",
+            "nome": "Emanuelly - Mendes"
+        },
+        {
+            "numero": "11",
+            "nome": "Michelli Trindade"
+        },
+        {
+            "numero": "12",
+            "nome": "Samia - @snfwr_"
+        },
+        {
+            "numero": "13",
+            "nome": "Tatty Araújo"
+        },
+        {
+            "numero": "14",
+            "nome": "Karen"
+        },
+        {
+            "numero": "15",
+            "nome": "Luciana - Madrinha"
+        },
+        {
+            "numero": "16",
+            "nome": "Luciana - Euclidelândia"
+        },
+        {
+            "numero": "17",
+            "nome": "Iris - Mendes"
+        },
+        {
+            "numero": "18",
+            "nome": "Izabelle"
+        },
+        {
+            "numero": "19",
+            "nome": "Gabriella Malhard"
+        },
+        {
+            "numero": "20",
+            "nome": "Felipe Sanches"
+        },
+        {
+            "numero": "21",
+            "nome": "Katherine - Mendes"
+        },
+        {
+            "numero": "22",
+            "nome": "Luciana - Madrinha"
+        },
+        {
+            "numero": "23",
+            "nome": "Letícia Coelho - @engenheira.coelho"
+        },
+        {
+            "numero": "24",
+            "nome": "Tatiana - Mendes"
+        },
+        {
+            "numero": "25",
+            "nome": "Cayo Rodrigues - @cayohenrique08"
+        },
+        {
+            "numero": "26",
+            "nome": "Heloísa - @encriptada"
+        },
+        {
+            "numero": "27",
+            "nome": "Tiago Frossard"
+        },
+        {
+            "numero": "28",
+            "nome": "Neide"
+        },
+        {
+            "numero": "29",
+            "nome": "Thamara"
+        },
+        {
+            "numero": "30",
+            "nome": "Luciana - Madrinha"
+        },
+        {
+            "numero": "31",
+            "nome": "Yumi - @DilemmaboyYuu"
+        },
+        {
+            "numero": "32",
+            "nome": "Romário Campos"
+        },
+        {
+            "numero": "33",
+            "nome": "Iris - Mendes"
+        },
+        {
+            "numero": "34",
+            "nome": "Thamara"
+        },
+        {
+            "numero": "35",
+            "nome": "Letícia Coelho - @engenheira.coelho"
+        },
+        {
+            "numero": "36",
+            "nome": "Letícia Faria"
+        },
+        {
+            "numero": "37",
+            "nome": "Luisa"
+        },
+        {
+            "numero": "38",
+            "nome": "Neucilene - Décio"
+        },
+        {
+            "numero": "39",
+            "nome": "Thaylan"
+        },
+        {
+            "numero": "40",
+            "nome": "Cacilda"
+        },
+        {
+            "numero": "41",
+            "nome": "Ana Lívia"
+        },
+        {
+            "numero": "42",
+            "nome": "Cayo Rodrigues - @cayohenrique08"
+        },
+        {
+            "numero": "43",
+            "nome": "Letícia Coelho - @engenheira.coelho"
+        },
+        {
+            "numero": "44",
+            "nome": "Marcos Silva"
+        },
+        {
+            "numero": "45",
+            "nome": "Adriana - Décio"
+        },
+        {
+            "numero": "46",
+            "nome": "Evandro - Euclidelândia"
+        },
+        {
+            "numero": "47",
+            "nome": "Simone"
+        },
+        {
+            "numero": "48",
+            "nome": "Davi"
+        },
+        {
+            "numero": "49",
+            "nome": "Victor da Conceição Moura"
+        },
+        {
+            "numero": "50",
+            "nome": "Cacilda"
+        },
+        {
+            "numero": "51",
+            "nome": "Victor da Conceição Moura"
+        },
+        {
+            "numero": "52",
+            "nome": "Angelo"
+        },
+        {
+            "numero": "53",
+            "nome": "Cláudio"
+        },
+        {
+            "numero": "54",
+            "nome": "Thais Patuelli"
+        },
+        {
+            "numero": "55",
+            "nome": "Tiago Frossard"
+        },
+        {
+            "numero": "56",
+            "nome": "Liliane - Euclidelândia"
+        },
+        {
+            "numero": "57",
+            "nome": "Giane"
+        },
+        {
+            "numero": "58",
+            "nome": "Leo Almeida"
+        },
+        {
+            "numero": "59",
+            "nome": "Thaylan"
+        },
+        {
+            "numero": "60",
+            "nome": "Josiane"
+        },
+        {
+            "numero": "61",
+            "nome": "Letícia - Prima"
+        },
+        {
+            "numero": "62",
+            "nome": "Thamara"
+        },
+        {
+            "numero": "63",
+            "nome": "Celinha - Décio"
+        },
+        {
+            "numero": "64",
+            "nome": "Michele Condack"
+        },
+        {
+            "numero": "65",
+            "nome": "Glaziela - Décio"
+        },
+        {
+            "numero": "66",
+            "nome": "Anna Clara - Euclidelândia"
+        },
+        {
+            "numero": "67",
+            "nome": "Sheila - Décio"
+        },
+        {
+            "numero": "68",
+            "nome": "Rose - @rose_queijos"
+        },
+        {
+            "numero": "69",
+            "nome": "Maike"
+        },
+        {
+            "numero": "70",
+            "nome": "Angelo"
+        },
+        {
+            "numero": "71",
+            "nome": "Sânia"
+        },
+        {
+            "numero": "72",
+            "nome": "Leila - Décio"
+        },
+        {
+            "numero": "73",
+            "nome": "Luciana - Euclidelândia"
+        },
+        {
+            "numero": "74",
+            "nome": "Andrey - Mendes"
+        },
+        {
+            "numero": "75",
+            "nome": "Heloísa - @fraldasmoderninhas"
+        },
+        {
+            "numero": "76",
+            "nome": "Henrique - Euclidelândia"
+        },
+        {
+            "numero": "77",
+            "nome": "Marta"
+        },
+        {
+            "numero": "78",
+            "nome": "Waldemiro"
+        },
+        {
+            "numero": "79",
+            "nome": "Letícia Faria"
+        },
+        {
+            "numero": "80",
+            "nome": "Cacilda"
+        },
+        {
+            "numero": "81",
+            "nome": "Priscila - Décio"
+        },
+        {
+            "numero": "82",
+            "nome": "Marcos Silva"
+        },
+        {
+            "numero": "83",
+            "nome": "Tiago Frossard"
+        },
+        {
+            "numero": "84",
+            "nome": "Felipe Sanches"
+        },
+        {
+            "numero": "85",
+            "nome": "Jefferson"
+        },
+        {
+            "numero": "86",
+            "nome": "Rachel - Décio (Mônica)"
+        },
+        {
+            "numero": "87",
+            "nome": "Angelo"
+        },
+        {
+            "numero": "88",
+            "nome": "Cayo Rodrigues - @cayohenrique08"
+        },
+        {
+            "numero": "89",
+            "nome": "Kristel - Décio"
+        },
+        {
+            "numero": "90",
+            "nome": "Chirley - Décio"
+        },
+        {
+            "numero": "91",
+            "nome": "Thamara"
+        },
+        {
+            "numero": "92",
+            "nome": "Thamara"
+        },
+        {
+            "numero": "93",
+            "nome": "Carlos - @carrlosv"
+        },
+        {
+            "numero": "94",
+            "nome": "Thais Miranda"
+        },
+        {
+            "numero": "95",
+            "nome": "Sânia"
+        },
+        {
+            "numero": "96",
+            "nome": "Karen"
+        },
+        {
+            "numero": "97",
+            "nome": "Carlos - @carrlosv"
+        },
+        {
+            "numero": "98",
+            "nome": "Carlos - @carrlosv"
+        },
+        {
+            "numero": "99",
+            "nome": "Julio Barbosa - @JulioBarbosa97"
+        },
+        {
+            "numero": "100",
+            "nome": "Felipe Sanches"
+        },
+        {
+            "numero": "101",
+            "nome": "Reydson"
+        },
+        {
+            "numero": "102",
+            "nome": "Jô - Décio"
+        },
+        {
+            "numero": "103",
+            "nome": "Leonardo Jardim"
+        },
+        {
+            "numero": "104",
+            "nome": "Sânia"
+        },
+        {
+            "numero": "105",
+            "nome": "Thauan"
+        },
+        {
+            "numero": "106",
+            "nome": "Felipe Sanches"
+        },
+        {
+            "numero": "107",
+            "nome": "Yumi - @DilemmaboyYuu"
+        },
+        {
+            "numero": "108",
+            "nome": "Kely - Décio"
+        },
+        {
+            "numero": "109",
+            "nome": "Thauan"
+        },
+        {
+            "numero": "110",
+            "nome": "Eliana - Décio"
+        },
+        {
+            "numero": "111",
+            "nome": "Janaína"
+        },
+        {
+            "numero": "112",
+            "nome": "Thaylan"
+        },
+        {
+            "numero": "113",
+            "nome": "Felipe Sanches"
+        },
+        {
+            "numero": "114",
+            "nome": "Ana Casimiro"
+        },
+        {
+            "numero": "115",
+            "nome": "Thauan"
+        },
+        {
+            "numero": "116",
+            "nome": "Vanessa - Arraial"
+        },
+        {
+            "numero": "117",
+            "nome": "Caio Tomé"
+        },
+        {
+            "numero": "118",
+            "nome": "Angelo"
+        },
+        {
+            "numero": "119",
+            "nome": "Andrea Claudia - Décio"
+        },
+        {
+            "numero": "120",
+            "nome": "Thamara"
+        },
+        {
+            "numero": "121",
+            "nome": "Arlete"
+        },
+        {
+            "numero": "122",
+            "nome": "Raphael Giron"
+        },
+        {
+            "numero": "123",
+            "nome": "Felipe Grion"
+        },
+        {
+            "numero": "124",
+            "nome": "Elenilda - Décio"
+        },
+        {
+            "numero": "125",
+            "nome": "Gudierre Biral"
+        },
+        {
+            "numero": "126",
+            "nome": "Sany - Décio"
+        },
+        {
+            "numero": "127",
+            "nome": "Bruno - Décio (Mônica)"
+        },
+        {
+            "numero": "128",
+            "nome": "Felipe Sanches"
+        },
+        {
+            "numero": "129",
+            "nome": "Caio - Décio"
+        },
+        {
+            "numero": "130",
+            "nome": "Silvania - Décio"
+        },
+        {
+            "numero": "131",
+            "nome": "Alan - Mendes"
+        },
+        {
+            "numero": "132",
+            "nome": "Léia - Décio"
+        },
+        {
+            "numero": "133",
+            "nome": "Raphael Gabrig"
+        },
+        {
+            "numero": "134",
+            "nome": "Agatha - Décio"
+        },
+        {
+            "numero": "135",
+            "nome": "Andrezza - Décio"
+        },
+        {
+            "numero": "136",
+            "nome": "Evelyn - @LynOnMars"
+        },
+        {
+            "numero": "137",
+            "nome": "Airis - Mendes"
+        },
+        {
+            "numero": "138",
+            "nome": "Walmir"
+        },
+        {
+            "numero": "139",
+            "nome": "Pollyana - Décio"
+        },
+        {
+            "numero": "140",
+            "nome": "Carlos - @carrlosv"
+        },
+        {
+            "numero": "141",
+            "nome": "Luciane"
+        },
+        {
+            "numero": "142",
+            "nome": "Anderson - Mendes"
+        },
+        {
+            "numero": "143",
+            "nome": "Patrícia - Mendes"
+        },
+        {
+            "numero": "144",
+            "nome": "Helena - Décio"
+        },
+        {
+            "numero": "145",
+            "nome": "Aline - @AlineComandulli"
+        },
+        {
+            "numero": "146",
+            "nome": "Altiva - Décio (Mônica)"
+        },
+        {
+            "numero": "147",
+            "nome": "Luciane"
+        },
+        {
+            "numero": "148",
+            "nome": "Letícia Coelho - @engenheira.coelho"
+        },
+        {
+            "numero": "149",
+            "nome": "Felipe Sanches"
+        },
+        {
+            "numero": "150",
+            "nome": "Luciane"
+        }
+    ]
+}
+
+function sortear() {
+
+    const min = 1
+    const max = 150
+
+    const sort = Math.floor((Math.random() * max) + min)
+
+    document.getElementById("numero").innerHTML = data.participantes[sort].numero;
+    document.getElementById("nome").innerHTML = data.participantes[sort].nome;
+}
